d6d93d205affbf5008cae5220a97b764807154f5
Yasser-Alnajjar <yasseralnajjar72@outlook.sa>
2023-07-25 14:04:46 +0300

    edit post,handle updatepost and render it in view

--- src/redux/slices/posts/postsSlice.js
+++ src/redux/slices/posts/postsSlice.js
@@ -43,29 +43,29 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded: {
-      //   reducer(state, action) {
-      //     state.posts.push(action.payload);
-      //   },
-      //   prepare(title, body, userId) {
-      //     return {
-      //       payload: {
-      //         id: userId,
-      //         userId,
-      //         title,
-      //         body,
-      //         date: new Date().toISOString(),
-      //         reactions: {
-      //           thumbsUp: 0,
-      //           wow: 0,
-      //           heart: 0,
-      //           rocket: 0,
-      //           coffee: 0,
-      //         },
-      //       },
-      //     };
-      //   },
-    },
+    // postAdded: {
+    //   reducer(state, action) {
+    //     state.posts.push(action.payload);
+    //   },
+    //   prepare(title, body, userId) {
+    //     return {
+    //       payload: {
+    //         id: userId,
+    //         userId,
+    //         title,
+    //         body,
+    //         date: new Date().toISOString(),
+    //         reactions: {
+    //           thumbsUp: 0,
+    //           wow: 0,
+    //           heart: 0,
+    //           rocket: 0,
+    //           coffee: 0,
+    //         },
+    //       },
+    //     };
+    //   },
+    // },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
       const existingPost = state.posts.find((post) => post.id === postId);
